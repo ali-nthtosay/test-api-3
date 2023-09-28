587b80e6127e7c4b043a430dcf953631ee6d4ed3
Amidpour <98856912+ali-nthtosay@users.noreply.github.com>
2023-09-28 12:56:37 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,56 @@
 <?php
 
+
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Basic GET Route
+Route::get('/api/resource', 'ApiController@getResource');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// POST Route
+Route::post('/api/resource', 'ApiController@createResource');
+
+// PUT or PATCH Route
+Route::put('/api/resource/{id}', 'ApiController@updateResource');
+
+// DELETE Route
+Route::delete('/api/resource/{id}', 'ApiController@deleteResource');
+
+// Route with Parameters
+Route::get('/api/users/{id}', 'UserController@show');
+
+// Route Group with Middleware
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Protected routes go here
+    Route::get('/api/protected', 'ProtectedController@index');
+    Route::post('/api/protected', 'ProtectedController@store');
+});
+
+// Resourceful Controller Routes
+Route::resource('api/posts', 'PostController');
+
+// Named Routes
+Route::get('/api/users', 'UserController@index')->name('users.index');
+
+// Route Prefixing
+Route::prefix('api/v1')->group(function () {
+    Route::get('/resource1', 'ApiController@resource1');
+    Route::get('/resource2', 'ApiController@resource2');
+});
+
+// Route Middleware
+Route::get('/api/sensitive', 'SensitiveController@index')->middleware('auth');
+
+// Route Model Binding
+Route::get('/api/posts/{post}', 'PostController@show');
+
+// Group with Namespace
+Route::namespace('Api')->group(function () {
+    Route::get('/api/resource', 'ApiController@getResource');
+});
+
+// API Versioning
+Route::prefix('v1')->group(function () {
+    Route::get('/api/resource', 'ApiController@getResource');
 });
